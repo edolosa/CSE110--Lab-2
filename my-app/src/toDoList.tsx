ea90246170a703b80a1be51d24d7a48138025033
Ciro Zhang <ciz001@ucsd.edu>
2024-10-14 13:40:34 -0700

    Exercise 1.5 Done

--- my-app/src/toDoList.tsx
+++ my-app/src/toDoList.tsx
@@ -3,8 +3,12 @@ import "./App.css";
 import { useState } from "react";
 import { GroceryItem } from "./types";
 import { dummyGroceryList } from "./constants";
+import { useParams } from "react-router-dom";
 
 export function ToDoList() {
+ const { name } = useParams();
+
+
  const [numRemainingItems, setNumRemainingItems] = useState(0);
 
  let [items, setItems] = useState(dummyGroceryList);
@@ -32,6 +36,7 @@ export function ToDoList() {
  return (
    <div className="App">
      <div className="App-body">
+     <h1>{name}'s To DList</h1>
        Items bought: {numRemainingItems}
        <form action=".">
          {items.map((item) => ListItem(item, handleCheckboxClick))}
